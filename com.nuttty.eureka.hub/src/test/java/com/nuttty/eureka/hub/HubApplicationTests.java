a25970c6deb9e939d91b81e8a92d961fef9e6412
jungbin97 <ljb5054@gmail.com>
2024-09-08 16:49:59 +0900

    Init: 프로젝트 생성 및 의존성 추가

---
+++ com.nuttty.eureka.hub/src/test/java/com/nuttty/eureka/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuttty.eureka.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
