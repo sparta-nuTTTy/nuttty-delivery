e5695c01ca050e35479ab8a851e407f6a6c7544e
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-09 22:25:21 +0900

    Refactor: "Unique 제약 조건 수정, 수정 메서드 추가"
    is_delete 로 인해 데이터베이스 단계에서 제약 조건 설정,
    수정 메서드 추가
    
    Resolves: #5

--- com.nuttty.eureka.hub/src/main/java/com/nuttty/eureka/hub/domain/model/Hub.java
+++ com.nuttty.eureka.hub/src/main/java/com/nuttty/eureka/hub/domain/model/Hub.java
@@ -1,5 +1,6 @@
 package com.nuttty.eureka.hub.domain.model;
 
+import com.nuttty.eureka.hub.presestation.request.HubRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -20,10 +21,10 @@ public class Hub extends AuditEntity{
     @Column(name = "hub_id")
     private UUID id;
 
-    @Column(unique = true)
+//    @Column(unique = true)
     private String name;
 
-    @Column(unique = true)
+//    @Column(unique = true)
     private String address;
     private String latitude;
     private String longitude;
@@ -37,5 +38,19 @@ public class Hub extends AuditEntity{
         this.latitude = latitude;
         this.longitude = longitude;
         this.userId = userId;
+    }
+
+    /**
+     * 허브 수정 메서드
+     * @param request
+     * @return
+     */
+    public Hub update(HubRequest request) {
+        this.name = request.getName();
+        this.address = request.getAddress();
+        this.latitude = request.getLatitude();
+        this.longitude = request.getLongitude();
+        this.userId = request.getUser_id();
+        return this;
     }
 }
