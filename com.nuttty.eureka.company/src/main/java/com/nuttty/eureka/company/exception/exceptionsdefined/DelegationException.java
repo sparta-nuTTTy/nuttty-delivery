ea076755e3fdbd32e9e83074550fe25212993c85
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-10 18:17:05 +0900

    Feat: "글로벌 예외 설정 및 로깅 설정"
    글로벌 예외 설정 및 로깅 설정
    
    Resolves: #8

---
+++ com.nuttty.eureka.company/src/main/java/com/nuttty/eureka/company/exception/exceptionsdefined/DelegationException.java
@@ -0,0 +1,7 @@
+package com.nuttty.eureka.company.exception.exceptionsdefined;
+
+public class DelegationException extends RuntimeException {
+    public DelegationException(String message) {
+        super(message);
+    }
+}
