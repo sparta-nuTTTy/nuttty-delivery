695a4ef821c32fd32b3f4f33ee1f7c5a0eda3e8b
atto08 <atto08@naver.com>
2024-09-11 22:39:12 +0900

    Feat: "회원 검색 구현"
    User - 마스터 관리자만 회원의 정보를 검색하는 기능 구현
    
    Resolves: #35

--- com.nuttty.eureka.auth/src/main/java/com/nuttty/eureka/auth/presentation/controller/UserController.java
+++ com.nuttty.eureka.auth/src/main/java/com/nuttty/eureka/auth/presentation/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.nuttty.eureka.auth.presentation.controller;
 
 import com.nuttty.eureka.auth.application.dto.UserInfoDto;
+import com.nuttty.eureka.auth.application.dto.UserSearchResponseDto;
+import com.nuttty.eureka.auth.application.service.AuthService;
 import com.nuttty.eureka.auth.application.service.UserService;
 import com.nuttty.eureka.auth.presentation.request.UserRoleUpdateRequestDto;
+import com.nuttty.eureka.auth.presentation.request.UserSearchRequestDto;
 import com.nuttty.eureka.auth.util.ResultResponse;
 import com.nuttty.eureka.auth.util.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     // 회원 상세 조회
     @GetMapping("/{user_id}")
@@ -49,6 +56,21 @@ public class UserController {
                 .data(userService.deleteUserInfo(role, targetUserId))
                 .resultCode(SuccessCode.DELETE_SUCCESS.getStatus())
                 .resultMessage(SuccessCode.DELETE_SUCCESS.getMessage())
+                .build();
+    }
+
+    // 회원 검색
+    @GetMapping("/search")
+    public ResultResponse<Page<UserSearchResponseDto>> searchUserInfo(@RequestHeader("X-User-Role") String role,
+                                                                      @PageableDefault Pageable pageable,
+                                                                      UserSearchRequestDto searchRequestDto) {
+
+        Page<UserSearchResponseDto> searchUserList = userService.searchUserInfo(role, pageable, searchRequestDto);
+
+        return ResultResponse.<Page<UserSearchResponseDto>>builder()
+                .data(searchUserList)
+                .resultCode(SuccessCode.SELECT_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.SELECT_SUCCESS.getMessage())
                 .build();
     }
 }
