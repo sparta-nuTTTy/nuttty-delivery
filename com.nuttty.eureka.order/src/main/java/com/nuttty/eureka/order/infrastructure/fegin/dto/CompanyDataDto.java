aed7c4eff68879fbc452175be29e735104aea477
jungbin97 <ljb5054@gmail.com>
2024-09-11 12:25:21 +0900

    Feat: "업체 조회 클라이언트 작성"
    Resolves: #20

---
+++ com.nuttty.eureka.order/src/main/java/com/nuttty/eureka/order/infrastructure/fegin/dto/CompanyDataDto.java
@@ -0,0 +1,18 @@
+package com.nuttty.eureka.order.infrastructure.fegin.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CompanyDataDto {
+    private UUID company;
+    private UUID userId;
+    private UUID hubId;
+    private String name;
+    private String type;
+    private String address;
+}
