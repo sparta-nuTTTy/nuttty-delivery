443985724cac62f5cfdafc86b131170567c6b83e
jungbin97 <ljb5054@gmail.com>
2024-09-15 02:28:25 +0900

    Feat: "QueryDSL JPA QueryFactory 설정"
    Resolves: #70

--- com.nuttty.eureka.order/src/main/java/com/nuttty/eureka/order/config/JpaConfig.java
+++ com.nuttty.eureka.order/src/main/java/com/nuttty/eureka/order/config/JpaConfig.java
@@ -1,9 +1,16 @@
 package com.nuttty.eureka.order.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "userAuditorAware")
 public class JpaConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
