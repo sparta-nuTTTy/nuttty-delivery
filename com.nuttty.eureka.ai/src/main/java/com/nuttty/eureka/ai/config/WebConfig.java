f8f311175ac1354d2076e69b07e9388d684374ce
jungbin97 <ljb5054@gmail.com>
2024-09-17 06:31:15 +0900

    Feat: "AI swagger 의존성 및 설정, CORS 문제 해결"
    Resolves: #99

---
+++ com.nuttty.eureka.ai/src/main/java/com/nuttty/eureka/ai/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.nuttty.eureka.ai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
+                .allowedOriginPatterns("http://localhost:*") // 모든 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "X-User-Id", "X-User-Role", "X-User-Email")
+                .allowCredentials(true);
+    }
+}
