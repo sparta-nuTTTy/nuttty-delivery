ef9c2a75a66e389aef51de644a6be4d9349207b7
jungbin97 <ljb5054@gmail.com>
2024-09-17 06:22:08 +0900

    Feat: "Company swagger 의존성 및 설정, CORS 문제 해결"
    Resolves: #99

---
+++ com.nuttty.eureka.company/src/main/java/com/nuttty/eureka/company/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.nuttty.eureka.company.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(title = "Company Service", version = "1.0.0", description = "Team nuTTTy의 Company Serivce입니다.")
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+                .addServersItem(new Server().url("/"));
+    }
+}
